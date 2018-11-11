ab6a454b1ec20ed6962cc956e99171a4eb1366a8
Arely Alcantara <arely0715@gmail.com>
2018-11-11 15:36:35 -0600

    things work or something

--- routes.js
+++ routes.js
@@ -36,17 +36,14 @@ module.exports = function (app) {
 		
 		User.findOne({ smuId: req.body.smuId }, function(err, user){
 			if (err) {
-				res.sendStatus(404);
-				res.send(err); 
+				res.status(404).send(err); 
 				//throw err; 
 			}
 			if (req.body.password.trim() == user.password.trim()) {
-				res.sendStatus(200);
-				res.send(user._id); 
+				res.status(200).send(user._id); 
 			} 
 			else {
-				res.sendStatus(500);
-				res.send('Invalid password'); 
+				res.status(500).send('Invalid password'); 
 			}
 		}
 		);
